4e1524f57ed9d09bceca40f425e5d71afc59de35
Giancarlos Garza <giancarlosgza@gmail.com>
2022-07-01 11:49:08 -0500

    GSAP animations for text and images

--- src/views/GsapAnimations.vue
+++ src/views/GsapAnimations.vue
@@ -1,38 +1,50 @@
 <template>
   <div>
-    <TheSidebar :is-sidebar-active="isSidebarActive"
-                :sidebar-toggle="toggleSidebar"/>
+    <TheSidebar
+        :is-sidebar-active="isSidebarActive"
+        :sidebar-toggle="toggleSidebar"
+    />
     <TheNavbar/>
 
     <div class="section">
       <div class="container">
         <div class="row">
-          <div class="col-12">
-            <h1>GSAP ANIMATIONS</h1>
-            <button class="btn btn-primary"
-                    @click="toggleSidebar()">
+          <div class="col-12 text-center">
+            <h1 class="text-hero text-gradient green">GSAP ANIMATIONS</h1>
+            <button class="btn btn-primary mt-3" @click="toggleSidebar()">
               <i class="material-icons-outlined">
-                {{ isSidebarActive ? 'close' : 'menu_open' }}
+                {{ isSidebarActive ? "close" : "menu_open" }}
               </i>
-              {{ isSidebarActive ? 'Close sidebar' : 'Open sidebar' }}
+              {{ isSidebarActive ? "Close sidebar" : "Open sidebar" }}
             </button>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="section">
+    <div class="section section-one">
       <div class="container">
         <div class="row">
-          <div class="col-12">
+          <div class="col-6">
             <i class="material-symbols-rounded icon-wrap mdc-38">&#xe8b6;</i>
-            <h2 class="fs-1">Far far away, behind the word mountains</h2>
+            <h2 class="fs-1 text-gradient green">
+              The smartest and fastest Pixel yet.
+            </h2>
             <h3>
-              macOS and M2 work together to bring more speed and responsiveness to all your go‑to apps — including
-              Microsoft 365 and your favorite iOS apps. Over 10,000 apps and plug‑ins are already optimized for Apple
-              silicon. And Rosetta 2 seamlessly translates apps designed for Intel processors for use on your MacBook
-              Pro.
+              macOS and M2 work together to bring more speed and responsiveness
+              to all your go‑to apps — including Microsoft 365 and your favorite
+              iOS apps. Over 10,000 apps and plug‑ins are already optimized for
+              Apple silicon. And Rosetta 2 seamlessly translates apps designed
+              for Intel processors for use on your MacBook Pro.
             </h3>
+          </div>
+          <div class="col-6">
+            <img
+                src="@/assets/img/keyboard.jpg"
+                loading="lazy"
+                class="img-fluid img-keyboard"
+                alt="Keyboard image"
+            />
           </div>
         </div>
       </div>
@@ -41,28 +53,24 @@
     <div class="section">
       <div class="container">
         <div class="row">
-          <div class="col-12">
+          <div class="col-6">
             <i class="material-symbols-rounded icon-wrap mdc-38">&#xf042;</i>
-            <h2 class="fs-1">Far far away, behind the word mountains</h2>
+            <h2 class="fs-1">
+              Google Tensor: Our first custom-built processor.
+            </h2>
             <h3>
-              Bring your photos, movies, music, and documents with you — and open them in a flash.
+              Bring your photos, movies, music, and documents with you — and
+              open them in a flash.
             </h3>
           </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="section">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
+          <div class="col-6">
             <i class="material-symbols-rounded icon-wrap mdc-38">&#xef76;</i>
-            <h2 class="fs-1">Far far away, behind the word mountains</h2>
+            <h2 class="fs-1">The most advanced smartphone camera.</h2>
             <h3>
-              Faster memory.
-              And more of it.
-              M2 has 100GB per second of memory bandwidth — 50 percent more than M1. And it supports 24GB of unified
-              memory for superfluid multitasking and working with massive files.
+              Faster memory. And more of it. M2 has 100GB per second of memory
+              bandwidth — 50 percent more than M1. And it supports 24GB of
+              unified memory for superfluid multitasking and working with
+              massive files.
             </h3>
           </div>
         </div>
@@ -72,13 +80,25 @@
     <div class="section">
       <div class="container">
         <div class="row">
-          <div class="col-12">
+          <div class="col-6">
+            <img
+                src="@/assets/img/pc.jpg"
+                loading="lazy"
+                class="img-fluid img-pc"
+                alt="Keyboard image"
+            />
+          </div>
+          <div class="col-6">
             <i class="material-symbols-rounded icon-wrap">&#xe145;</i>
-            <h2 class="fs-1">Far far away, behind the word mountains</h2>
+            <h2 class="fs-1 text-gradient blue">
+              Redesigned for powerful performance.
+            </h2>
             <h3>
-              The high-performance media engine on M2 supports ProRes encode and decode. So you can play and edit up to
-              11 streams of 4K ProRes video and up to 2 streams of 8K ProRes video — including 4K ProRes video shot on
-              iPhone 13 Pro.12 And convert projects to ProRes up to 3x faster than before.13
+              The high-performance media engine on M2 supports ProRes encode and
+              decode. So you can play and edit up to 11 streams of 4K ProRes
+              video and up to 2 streams of 8K ProRes video — including 4K ProRes
+              video shot on iPhone 13 Pro.12 And convert projects to ProRes up
+              to 3x faster than before.13
             </h3>
           </div>
         </div>
@@ -86,17 +106,19 @@
     </div>
 
     <section class="section section-hero">
-      <div id="section-1" class="container">
+      <div class="container">
         <div class="row">
           <div class="col-12 text-center">
-            <h2 class="fs-1 text-muted">THE MOST POWERFUL</h2>
-            <h2 class="fs-1">CSS AND JS ANIMATIONS</h2>
+            <h2 class="text-hero is-separated">
+              Powerful <span class="text-gradient green">CSS</span> and
+              <span class="text-gradient gold">JS</span> animations
+            </h2>
           </div>
         </div>
       </div>
     </section>
 
-    <section class="section">
+    <section class="section section-cards">
       <div class="container">
         <div class="row">
           <div class="col-4">
@@ -127,49 +149,102 @@
       </div>
     </section>
 
+    <section class="section">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6 text-center">
+            <h2 class="fs-1">Grab-and-go accessories.</h2>
+            <img
+                src="@/assets/img/laptop.jpg"
+                loading="lazy"
+                class="img-fluid img-laptop mt-3"
+                width="800p"
+                height="800"
+                alt="Laptop image"
+            />
+            <p class="mt-3">
+              <a href="#" class="text-link">Shop all Pixel 6 Pro accessories</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 
 <script>
 import TheSidebar from "@/components/TheSidebar";
 import TheNavbar from "@/components/TheNavbar";
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 export default {
   name: "GsapAnimations",
   components: {TheNavbar, TheSidebar},
   data() {
     return {
-      isSidebarActive: true
-    }
+      isSidebarActive: true,
+    };
   },
   mounted() {
-    this.sectionsAnimate()
+    this.sectionsAnimate();
   },
   methods: {
     toggleSidebar() {
-      this.isSidebarActive = !this.isSidebarActive
+      this.isSidebarActive = !this.isSidebarActive;
     },
     sectionsAnimate() {
-      gsap.registerPlugin(ScrollTrigger)
-      gsap.to('.section-hero', {
+      gsap.registerPlugin(ScrollTrigger);
+      gsap.to(".img-keyboard", {
         scrollTrigger: {
-          trigger: '.section-hero',
-          toggleActions: 'restart pause resume none',
+          trigger: ".img-keyboard",
+          toggleActions: "resume pause resume none",
+          start: "center bottom",
         },
-        backgroundColor: "#ffffff",
-        duration: 3
-      })
-      gsap.to('.card', {
+        width: "90%",
+        borderRadius: "100px 0 100px 0",
+        duration: 2,
+      });
+      gsap.to(".img-pc", {
         scrollTrigger: {
-          trigger: '.card',
-          toggleActions: 'restart pause resume none'
+          trigger: ".img-pc",
+          toggleActions: "resume pause resume none",
+          start: "center center",
         },
-        transform: 'translateY(-50px)',
-        duration: 3
-      })
-    }
-  }
-}
+        width: "90%",
+        borderRadius: "100px 0 100px 0",
+        duration: 2,
+      });
+      gsap.to(".is-separated", {
+        scrollTrigger: {
+          trigger: ".is-separated",
+          toggleActions: "resume pause resume none",
+          start: "center center",
+        },
+        ease: 'ease-in-out',
+        letterSpacing: "-0.05em",
+        duration: 1,
+      });
+      gsap.to(".section-hero", {
+        scrollTrigger: {
+          trigger: ".section-hero",
+          toggleActions: "restart pause resume none",
+        },
+        backgroundColor: "#000000",
+        color: "#ffffff",
+        duration: 3,
+      });
+      gsap.to(".card", {
+        scrollTrigger: {
+          trigger: ".card",
+          toggleActions: "restart pause resume none",
+        },
+        transform: "translateY(-80px)",
+        borderRadius: "25px",
+        duration: 1,
+      });
+    },
+  },
+};
 </script>
